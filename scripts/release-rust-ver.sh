4e67ccffbe9e11244d85d843b955a12250970c30
Ignacio Duart <iduartgomez@gmail.com>
2023-09-21 16:05:53 +0200

    Add release script

---
+++ scripts/release-rust-ver.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR && cd ..
+SRC_DIR=$(pwd)
+source typescript/package.prod.sh
+cd $SRC_DIR
+
+cargo publish --dry-run -p freenet-macros || { exit 1; }
+read -p "Publish freenet-macros? " -n 1 -r
+echo   
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cargo publish -p freenet-macros
+else 
+	echo "Not publishing freenet-macros"
+fi
+
+cargo publish --dry-run -p freenet-stdlib || { exit 1; }
+read -p "Publish freenet-stdlib? " -n 1 -r
+echo   
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cargo publish -p freenet-stdlib
+else 
+	echo "Not publishing freenet-stdlib"
+fi
