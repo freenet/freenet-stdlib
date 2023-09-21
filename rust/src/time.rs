2db69a981feb733cf60ab0fb18abc13721481db0
Ignacio Duart <iduartgomez@gmail.com>
2023-09-21 12:07:57 +0200

    Fix host exports naming

--- rust/src/time.rs
+++ rust/src/time.rs
@@ -8,14 +8,14 @@ pub fn now() -> DateTime<Utc> {
     let mut uninit = MaybeUninit::<chrono::DateTime<Utc>>::uninit();
     let ptr = uninit.as_mut_ptr() as usize as i64;
     unsafe {
-        __loc__time__utc_now(crate::global::INSTANCE_ID, ptr);
+        __frnt__time__utc_now(crate::global::INSTANCE_ID, ptr);
         uninit.assume_init();
         std::mem::transmute(uninit)
     }
 }
 
-#[link(wasm_import_module = "locutus_time")]
+#[link(wasm_import_module = "freenet_time")]
 extern "C" {
     #[doc(hidden)]
-    fn __loc__time__utc_now(id: i64, ptr: i64);
+    fn __frnt__time__utc_now(id: i64, ptr: i64);
 }
