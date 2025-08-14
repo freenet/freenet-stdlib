9d7650b93c5e2e6453a2a3d965130a31fad09729
plein <68096727+planetoryd@users.noreply.github.com>
2025-08-14 08:32:37 +0800

    logging in contract and non contract env

--- rust/src/log.rs
+++ rust/src/log.rs
@@ -1,3 +1,13 @@
+#[macro_export]
+macro_rules! info {
+    ($fmt:expr, $($args:tt)*) => {
+        #[cfg(not(feature="contract"))]
+        tracing::info!($fmt, $($args)*);
+        #[cfg(feature="contract")]
+        info(&format!($fmt, $($args)*));
+    };
+}
+
 pub fn info(msg: &str) {
     let ptr = msg.as_ptr() as _;
     unsafe {
@@ -9,4 +19,14 @@ pub fn info(msg: &str) {
 extern "C" {
     #[doc(hidden)]
     fn __frnt__logger__info(id: i64, ptr: i64, len: i32);
+}
+
+#[test]
+fn log_non_contract() {
+    use tracing::level_filters::LevelFilter;
+
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(LevelFilter::INFO)
+        .init();
+    info!("n={}, y={:?}", 1, 2);
 }
